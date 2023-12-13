9abba0c7ec1f061d3ee8e83c2fe6ce824b8938b8
IgorOlikov <i.olikov@mail.ru>
2023-12-14 04:05:46 +0500

    add route render view

--- core/Application.php
+++ core/Application.php
@@ -6,16 +6,17 @@ namespace app\core;
 class Application
 {
 
-    public  Router $router;
+    public Router $router;
+    public Request $request;
+
         public function __construct()
         {
-            $this->router = new Router();
-
+            $this->request = new Request();
+            $this->router = new Router($this->request);
         }
-
         public function run()
         {
-            //todo
+          echo  $this->router->resolve();
         }
 
 
