cf2da8fe042f1d07dc52dbb2e1b7ae298c004d71
IgorOlikov <i.olikov@mail.ru>
2023-12-17 01:33:03 +0500

    unique email validation rule

--- core/Model.php
+++ core/Model.php
@@ -11,6 +11,8 @@ abstract class Model
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
 
+    public const RULE_UNIQUE = 'unique';
+
 
 
     public function loadData($data)
@@ -52,6 +54,18 @@ abstract class Model
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}){
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
+                if ($ruleName === self::RULE_UNIQUE){
+                    $className = $rule['class'];
+                    $uniqueAttr = $rule['attribute'] ?? $attribute;
+                    $tableName = $className::tableName();
+                    $statement = Application::$app->db->prepare("SELECT * FROM $tableName WHERE $uniqueAttr = :attr");
+                    $statement->bindValue(":attr", $value);
+                    $statement->execute();
+                    $record = $statement->fetchObject();
+                    if ($record){
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                    }
+                }
             }
         }
         return empty($this->errors);
@@ -74,6 +88,7 @@ abstract class Model
             self::RULE_MIN => 'Min length must be {min}',
             self::RULE_MAX => 'Max length must be {max}',
             self::RULE_MATCH => 'This filed must be the same as {match}',
+            self::RULE_UNIQUE => 'Record with this {field} already exists',
         ];
     }
 
